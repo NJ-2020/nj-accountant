c46698cbc16b9b292b6c184a05268d83dc99adaf
NJ-2020 <ntt.london@gmail.com>
2023-02-03 07:19:14 +0700

    improve seo

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 // Fetch Data Featured speakers
-const speaker = document.querySelector(".speaker-list");
+// const speaker = document.querySelector(".speaker-list");
 /*
 const speakersobject = [
   {
@@ -11,40 +11,40 @@ const speakersobject = [
     description:
       "Holds MA in Banking and Insurance - London.  Qalified ACCA in 2003 -London- and has been working in accountancy practicen ever since..",
   },
-  // {
-  //   id: 12,
-  //   image: "images/img1.jpg",
-  //   name: "Khadijah Mella",
-  //   title: "The best United Kingdom Jockey in 2020",
-  //   description:
-  //     "The best United Kingdom jockey.  Learns from scracth within only 6 weeks before winning",
-  // },
+  {
+    id: 12,
+    image: "images/img1.jpg",
+    name: "Khadijah Mella",
+    title: "The best United Kingdom Jockey in 2020",
+    description:
+      "The best United Kingdom jockey.  Learns from scracth within only 6 weeks before winning",
+  },
 ];
 */
 
-function loadSpeakers() {
-  speakersobject.forEach(({ id, name, title, description, image }) => {
-    const li = document.createElement("li");
-    const itemBody = `<li class='speaker-item'>
-       <img
-         class='thumbnail'
-         src='${image}'
-         alt='${name} ${id}'
-       />
-       <div>
-         <h6 class='heading-3'>${name}</h6>
-         ${
-           title
-             ? `<small class='caption-1 fs-italics fw-bold color-red'>${title}</small>`
-             : ""
-         }
-         <hr class='line-gray'>
-         <p class='caption-1'>${description}</p>
-       </div>
-     </li>`;
-    li.innerHTML = itemBody;
-    speaker.appendChild(li);
-  });
-}
+// function loadSpeakers() {
+//   speakersobject.forEach(({ id, name, title, description, image }) => {
+//     const li = document.createElement("li");
+//     const itemBody = `<li class='speaker-item'>
+//        <img
+//          class='thumbnail'
+//          src='${image}'
+//          alt='${name} ${id}'
+//        />
+//        <div>
+//          <h6 class='heading-3'>${name}</h6>
+//          ${
+//            title
+//              ? `<small class='caption-1 fs-italics fw-bold color-red'>${title}</small>`
+//              : ""
+//          }
+//          <hr class='line-gray'>
+//          <p class='caption-1'>${description}</p>
+//        </div>
+//      </li>`;
+//     li.innerHTML = itemBody;
+//     speaker.appendChild(li);
+//   });
+// }
 
-loadSpeakers();
+// loadSpeakers();
