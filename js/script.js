7d8b5d2508b78df50757d81cef8a5cccecc29ef9
NJ-2020 <ntt.london@gmail.com>
2023-03-01 15:36:23 +0700

    close the menu toggle when click li

--- js/script.js
+++ js/script.js
@@ -2,9 +2,17 @@
 const menu = document.querySelector('#mobile-menu');
 const body = document.querySelector('#body');
 const menulinks = document.querySelector('.nav-menu');
-
+const menuLi = document.querySelectorAll('.nav-menu li');
 menu.addEventListener('click', () => {
   menu.classList.toggle('is-active');
   body.classList.toggle('blur');
   menulinks.classList.toggle('active');
 });
+
+menuLi.forEach((li) => {
+  li.addEventListener("click", () => {
+    menu.classList.remove('is-active');
+    body.classList.remove('blur');
+    menulinks.classList.remove('active');
+  })
+})
